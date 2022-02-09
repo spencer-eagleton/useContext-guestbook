a82bddd034b750d62ba25d7dce31f1f81efb7ccf
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-02-09 12:29:32 -0800

    created useMessages hook

--- src/context/EntryContext.jsx
+++ src/context/EntryContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 const EntryContext = createContext()
 
@@ -6,4 +6,12 @@ export const EntryProvider = ({ children }) => {
     const [messages, setMessages] = useState([])
     const value = {messages, setMessages}
     return <EntryContext.Provider value={value}>{children}</EntryContext.Provider>
+}
+
+export const useMessages = () => {
+    const context = useContext(EntryContext) 
+    if (context === undefined) {
+        throw new Error('must use useMessages in EntryContext provider')
+    }
+    return context
 }
