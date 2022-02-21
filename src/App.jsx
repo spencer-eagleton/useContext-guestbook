a1a8d656464434e0fa65e94ee4903d24250af4cf
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-02-21 15:09:40 -0800

    added browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import './App.css'
+import { BrowserRouter, Switch } from 'react-router-dom';
 import { EntryProvider } from "./context/EntryContext";
 import { UserProvider } from "./context/UserContext";
 import { useTheme } from "./hooks/useTheme";
 import Home from "./views/Home/Home";
 
+
 export default function App() {
   const { theme, toggleTheme } = useTheme();
   return (
     <div className={`App ${theme}`}>
+    <BrowserRouter>
+    
+      <button type="button" onClick={toggleTheme}>switch wall</button>
+      <Switch>
 
-    <button type="button" onClick={toggleTheme}>switch wall</button>
-  <UserProvider>
-    <EntryProvider>
-    <Home />
-    </EntryProvider>
-    </UserProvider>
+      <UserProvider>
+        <EntryProvider>
+          <Home />
+        </EntryProvider>
+      </UserProvider>
+    
+      </Switch>
+    </BrowserRouter>
 
     </div>
 
